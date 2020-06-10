556797118374f8d6293e5f18aec3f693c4e3d9e7
Taylor Swanson <kaleidoflowers@gmail.com>
2020-06-10 16:09:58 -0600

    Removed old code from addNode

--- addNode.sh
+++ addNode.sh
@@ -17,58 +17,13 @@ fi
 # Node needs a random name to prevent collisions
 INSTANCENAME=`openssl rand -hex 5`
 INSTANCENAME="x$INSTANCENAME"
-# # This is where we will mount the fs of each node
-# MOUNTPOINT="$BASEDIR/dev-mounts/$INSTANCENAME"
-
-
-# # We do this so the node version is always the same
-# CACHEPATH="$BASEDIR/dev-cache/"
-# mkdir -p $CACHEPATH
-
-# INSTALLERURL="https://deb.nodesource.com/setup_14.x"
-# INSTALLNAME="setup_14.sh"
-# INSTALLSCRIPTPATH="$CACHEPATH/$INSTALLNAME"
-
-
-# if [ -f "$CACHEPATH/$INSTALLNAME" ]; then
-#   # echo "Using existing install script $INSTALLNAME"
-#   echo ""
-# else
-#   echo "Downloading install script from $INSTALLERURL"
-
-#   curl -SLsf "https://deb.nodesource.com/setup_14.x" > $INSTALLSCRIPTPATH
-# fi
 
 # # Do the magic with multipass
 multipass launch -vv -d 512M -m 360M -n "$INSTANCENAME" --cloud-init ./dev_cloud_init.yml
-# mkdir -p $MOUNTPOINT
 
 # Send this archive over to new instance
 NEWIP=$(multipass list | grep $INSTANCENAME | awk '{print $3}')
-echo "New node at $NEWIP"
+echo "New node created:"
 
-# # Mount, then make sure mount is at home
-# # Mount directory must not exist already
-# # multipass mount $MOUNTPOINT $INSTANCENAME:/home/ubuntu/host
-# echo "Node mounted at $MOUNTPOINT at remote /home/ubuntu/host"
-
-# echo "Installing deps"
-# cp $INSTALLSCRIPTPATH "$MOUNTPOINT/$INSTALLNAME"
-# multipass exec $INSTANCENAME -- sudo chmod +x /home/ubuntu/host/$INSTALLNAME
-# multipass exec $INSTANCENAME -- sudo /home/ubuntu/host/$INSTALLNAME
-# multipass exec $INSTANCENAME -- rm /home/ubuntu/host/$INSTALLNAME
-# multipass exec $INSTANCENAME -- sudo apt-get install -y nodejs
-
-# echo "Building ts..."
-# cd $BASEDIR/host/
-# npm run build-ts
-# cd $BASEDIR
-
-# # echo "Copying host code"
-# # cp -r $BASEDIR/host/* $MOUNTPOINT/host/
-
-echo "Created"
-
-
-# IMPORTANT last line must be the ip address of the server
+# IMPORTANT last line must be the ip address and name of the server
 echo $NEWIP $INSTANCENAME
